06d5fe3a0a5b5d18c4e3592fe0d67eb80fd7123b
Naim Hossain <najmulspark@gmail.com>
2022-06-23 16:57:20 +0600

    Multer install for multiple form date

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var multer = multer();
 
 app = express();
+app.use(bodyParser.json());
+app.use(multer.array());
+app.use(express.static('public'));
+
 
 app.get('/', function(req, res){
-  res.send("Hello Express Js");
+  res.send("Home Page");
 });
+
+app.get('/about', function(req, res){
+  res.send("About Page");
+});
+
+
+app.get('/query', function(req, res){
+   const name = req.query.name;
+   console.log(req.query);
+   res.end(name);
+});
+
+app.post('/post', function(req, res){
+  console.log(req.body);
+  const name = req.body.name;
+  const age = req.body.age;
+  res.send(name +" " + age);
+});
+
 
 app.listen(8000, function(){
   console.log(`Server run with port: 8000`);
