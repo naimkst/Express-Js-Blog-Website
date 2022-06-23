2a6ec44a659c8585b80ae4018ae89345bf7980e5
Naim Hossain <najmulspark@gmail.com>
2022-06-23 17:34:03 +0600

    File Uplaod function done

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var multer = multer();
+var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+const { v4: uuidv4 } = require('uuid');
+// var multer = multer();
 
 app = express();
 app.use(bodyParser.json());
-app.use(multer.array());
-app.use(express.static('public'));
+// app.use(multer.array());
+// app.use(express.static("public"));
 
-
-app.get('/', function(req, res){
+app.get("/", function (req, res) {
   res.send("Home Page");
 });
 
-app.get('/about', function(req, res){
+app.get("/about", function (req, res) {
   res.send("About Page");
 });
 
-
-app.get('/query', function(req, res){
-   const name = req.query.name;
-   console.log(req.query);
-   res.end(name);
+app.get("/query", function (req, res) {
+  const name = req.query.name;
+  console.log(req.query);
+  res.end(name);
 });
 
-app.post('/post', function(req, res){
-  console.log(req.body);
-  const name = req.body.name;
-  const age = req.body.age;
-  res.send(name +" " + age);
+const storage = multer.diskStorage({
+  destination: function (req, file, callBack) {
+    callBack(null, "./uploads");
+  },
+  filename: function (req, file, callBack) {
+    const fileName = uuidv4() + file.originalname.replace(/\s/g, '');
+    callBack(null, fileName);
+  },
+
 });
 
+var upload = multer({ storage: storage }).single("myfile");
+app.post("/post", function (req, res) {
+  upload(req, res, function(error){
+    const imageName = req.file.originalname;
+    if(error){
+      res.send("File Upload Field");
+    }else{
+      res.send("File upload success");
+    }
+  });
+});
 
-app.listen(8000, function(){
+app.listen(8000, function () {
   console.log(`Server run with port: 8000`);
 });
